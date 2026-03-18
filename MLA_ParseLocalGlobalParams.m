264fd802a409f300570065f486ce80c7c4dede64
YHT-22 <12118042@zju.edu.cn>
2026-03-18 21:02:03 +0800

    202603181

---
+++ MLA_ParseLocalGlobalParams.m
@@ -0,0 +1,14 @@
+function MSTIparams = MLA_ParseLocalGlobalParams(ProtocolStr)
+
+% get params
+MSTIparams.Protocol = ProtocolStr;
+MSTIparams.Window = [-500, 1200];
+MSTIparams.ICAWindow = [-500, 1200];
+MSTIparams.plotWin = [0, 500];
+MSTIparams.compareWin = [0, 500];
+MSTIparams.sigTestWin = [0, 200];
+MSTIparams.BaselineWin = [-100, 0];
+MSTIparams.chPlotFcn = @MLA_PlotRasterLfp_LocalGlobalProcess;
+MSTIparams.sigTestMethod = "ttest2";
+
+end
---
+++ PassiveProcess_LocalGlobalTemp.m
@@ -0,0 +1,64 @@
+function trialAll = PassiveProcess_LocalGlobalTemp(epocs)
+%check ICI sequence
+ExcludeICIIdx  = find(roundn(epocs.ICI0.data, -1) == 0);
+epocs.ICI0.data(ExcludeICIIdx) = [];
+epocs.ICI0.onset(ExcludeICIIdx) = [];
+epocs.ICI0.offset(ExcludeICIIdx) = [];
+epocs.ordr.data(ExcludeICIIdx) = [];
+epocs.ordr.onset(ExcludeICIIdx) = [];
+epocs.ordr.offset(ExcludeICIIdx) = [];
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+trialOnset = epocs.Swep.onset * 1000; % ms
+ICIOnset = epocs.ICI0.onset * 1000; % ms
+ICIAll = epocs.ICI0.data;
+trialAll = [];
+cellArrayAll = [];
+for tIdx = 1 : length(trialOnset)
+    devordr = [];
+    if tIdx < length(trialOnset)
+        trialtimeIdx = find(ICIOnset >= trialOnset(tIdx) & ICIOnset < trialOnset(tIdx + 1));
+    else
+        trialtimeIdx = find(ICIOnset >= trialOnset(tIdx));
+    end
+    onIdx    = find([0; diff(ICIAll(trialtimeIdx))]);
+    InsertICI   = ICIAll(trialtimeIdx(onIdx(1)));
+    BaseICI    = ICIAll(trialtimeIdx(onIdx(2)));
+
+    ICIOnsetTemp = ICIOnset(trialtimeIdx);
+    InsertIdx = find(ICIAll(trialtimeIdx) == InsertICI);
+    IdxTemp = [find([0; diff(InsertIdx)] ~= 1);numel(InsertIdx)+1];
+    Insert_Num = diff(IdxTemp);
+    Insert_Time = ICIOnsetTemp(InsertIdx(IdxTemp(1:end-1)));
+    trialNum      = (tIdx - 1) * numel(Insert_Num) + (1 : numel(Insert_Num))';
+    if BaseICI < InsertICI
+        trialType = cellfun(@(InNum) strcat(string(BaseICI), '_', string(InsertICI), '_Inc_N', string(InNum)), ...
+                            mat2cell(Insert_Num, ones([size(Insert_Num, 1), 1])));
+        for i = 1:length(Insert_Num)
+            devordr(i, 1) = find(Insert_Num(i) == sort(Insert_Num));
+        end
+    elseif BaseICI > InsertICI
+        trialType = cellfun(@(InNum) strcat(string(BaseICI), '_', string(InsertICI), '_Dec_N', string(InNum)), ...
+                            mat2cell(Insert_Num, ones([size(Insert_Num, 1), 1])));
+        for i = 1:length(Insert_Num)
+            devordr(i, 1) = find(Insert_Num(i) == sort(Insert_Num)) + numel(Insert_Num);
+        end
+    end
+
+    % construct struct
+    matrixCell = num2cell([trialNum, Insert_Time, devordr, ...
+                    repmat(InsertICI, numel(trialNum), 1), ...
+                    repmat(BaseICI, numel(trialNum), 1), ...
+                    Insert_Num]);
+    strCell = arrayfun(@(x) x, trialType, 'UniformOutput', false);
+    cellArrayTemp = [matrixCell, strCell];
+    cellArrayAll = [cellArrayAll; cellArrayTemp];
+
+end
+    trialAll      = cell2struct([cellArrayAll(:, 1), cellArrayAll(:, 2), cellArrayAll(:, 3), ...
+                                cellArrayAll(:, 4), cellArrayAll(:, 5), cellArrayAll(:, 6), ...
+                                cellArrayAll(:, 7)], ... 
+                                {'trialNum', 'soundOnsetSeq', 'devOrdr', 'InsertICI', 'BaseICI', 'InsertNum', 'trialType'}, 2); 
+end
+
+
