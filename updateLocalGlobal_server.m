9840c09f12c51a484f92e33581c0e8c2c6dedd93
YHT <12118042@zju.edu.cn>
2026-03-20 17:26:10 +0800

    update 2026-03-20 17:26:10 by yht

--- updateLocalGlobal_server.m
+++ updateLocalGlobal_server.m
@@ -1,10 +1,6 @@
-function updateLocalGlobal_server(SyncOption)
-narginchk(0, 1);
-if nargin < 1
-    SyncOption = false;
-end
-opts.log                  = '';
-opts.SyncOption           = SyncOption;
-opts.RepositoryPaths      = fileparts(mfilename("fullpath"));
+function updateLocalGlobal_server
 
-mu.syncRepositories(opts);
+mu.syncRepositories( ...
+    "RepositoryPaths", fileparts(mfilename("fullpath")) ...
+);
+
