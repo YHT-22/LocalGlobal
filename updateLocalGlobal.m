0925d903f435695f600432af9224807a5d7eb281
YHT <12118042@zju.edu.cn>
2026-03-20 16:26:52 +0800

    updateLocalGlobal

--- updateLocalGlobal.m
+++
@@ -1,6 +0,0 @@
-function updateLocalGlobal(SyncOption)
-narginchk(0, 1);
-if nargin < 1
-    SyncOption = false;
-end
-mu.syncRepositories([], "RepositoryPaths", fileparts(mfilename("fullpath")), "SyncOption", SyncOption);
