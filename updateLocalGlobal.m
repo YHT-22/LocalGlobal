24a619bd6a224a34063fba02901f8e654d03a57a
YHT-22 <12118042@zju.edu.cn>
2026-03-20 15:30:52 +0800

    update 2026-03-20 15:30:52 by yht

--- updateLocalGlobal.m
+++ updateLocalGlobal.m
@@ -1,6 +1,9 @@
-function updateLocalGlobal(SyncOption)
-narginchk(0, 1);
+function updateLocalGlobal(SyncOption, RepositoryPaths)
+narginchk(0, 2);
 if nargin < 1
     SyncOption = false;
 end
-mu.syncRepositories([], "RepositoryPaths", fileparts(mfilename("fullpath")), "SyncOption", SyncOption);
+if nargin < 2
+    RepositoryPaths = fileparts(mfilename("fullpath"));
+end
+mu.syncRepositories([], "RepositoryPaths", RepositoryPaths, "SyncOption", SyncOption);
